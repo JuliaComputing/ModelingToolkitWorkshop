b79c74f046f50d714f5b76e6ef94365ca3bd332f
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2022-10-28 13:28:23 +0300

    data as csv

--- solutions/problem4.jl
+++ solutions/problem4.jl
@@ -3,7 +3,7 @@ using JuliaSimModelOptimizer
 using ModelingToolkit
 using OrdinaryDiffEq
 using Unitful
-using DataFrames
+using DataFrames, CSV
 using OptimizationOptimJL
 using DataInterpolations
 
@@ -14,18 +14,7 @@ n_days = 5.0
 t0 = 2.0 * day
 tend = t0 + n_days
 
-pre_trial = Trial(nothing, model; tspan=(t0, tend), alg=QNDF(autodiff=false))
-
-pre_invprob = InverseProblem([pre_trial], model,
-    [
-        @nonamespace(model.T_fluids_1₊k) => (270.0, 280.0),
-        @nonamespace(model.T_fluids_2₊k) => (270.0, 280.0),
-        @nonamespace(model.T_fluids_3₊k) => (270.0, 280.0)
-    ]
-)
-
-τ = ustrip(u"K", 5.0u"°C")
-data = DataFrame(solve_trial(pre_trial, fill(τ, 3), pre_invprob))
+data = CSV.read(joinpath(@__DIR__, "..", "data", "building_data.csv"), DataFrame)
 
 trial = Trial(data, model; tspan=(t0, tend), alg=QNDF(autodiff=false))
 
